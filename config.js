53aac7d40067941f74af4e7719a379793f636767
khoikutefc1 <85389090+khoikutefc1@users.noreply.github.com>
2022-01-10 23:13:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,9 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    titleWeb: "Heyy baby im Nhật Anh",
+    introTitle: 'Send to Dunggg',
+    introDesc: `Hôm nay là ngày zui zui và anh có đôi lời muốn nói..
+    I love u babe
+    love u so `,
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
